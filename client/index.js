bbd7e81325c9fe4afed6df02d5b348ff7b279369
AdrienPiombini <adrien.piombini@gmail.com>
2024-07-15 22:13:19 +0200

    add merkltree proof

--- client/index.js
+++ client/index.js
@@ -1,13 +1,13 @@
-const axios = require('axios');
-const niceList = require('../utils/niceList.json');
-const MerkleTree = require('../utils/MerkleTree');
-
-const serverUrl = 'http://localhost:1225';
+const axios = require("axios");
+const prompt = require("prompt-sync")();
+const serverUrl = "http://localhost:1225";
 
 async function main() {
-  // TODO: how do we prove to the server we're on the nice list? 
+  // TODO: how do we prove to the server we're on the nice list?
+  const username = prompt("Enter a name to see if you are on the list : ");
 
   const { data: gift } = await axios.post(`${serverUrl}/gift`, {
+    username,
     // TODO: add request body parameters here!
   });
 
